922e1734351bf1e0a231d73901d6b73e598458a2
extezo <vow.mazin@yandex.ru>
2021-09-25 16:57:20 +0300

    makefile

--- lab1/CreatePersons.c
+++ lab1/CreatePersons.c
@@ -8,16 +8,16 @@ void CreatePersons(int argc, char *argv[], struct Person persons[],
 		persons[i].ln = "";
 		persons[i].age = 0;
 	}
-	for (int i = 1, count = 0; i < argc; i++) {
+	for (int i = 1, count = -1; i < argc; i++) {
 			if (CompareStrings(argv[i], "-person") == 0)
 				count++;
 			else if (CompareStrings(argv[i], "--fn") == 0 && argv[i + 1][0] != '-')
-				persons[count - 1].fn = argv[i + 1];
+				persons[count].fn = argv[i + 1];
 			else if (CompareStrings(argv[i], "--mn") == 0 && argv[i + 1][0] != '-')
-				persons[count - 1].mn = argv[i + 1];
+				persons[count].mn = argv[i + 1];
 			else if (CompareStrings(argv[i], "--ln") == 0 && argv[i + 1][0] != '-')
-				persons[count - 1].ln = argv[i + 1];
+				persons[count].ln = argv[i + 1];
 			else if (CompareStrings(argv[i], "--age") == 0 && argv[i + 1][0] != '-')
-				persons[count - 1].age = atoi(argv[i + 1]);
+				persons[count].age = atoi(argv[i + 1]);
 	}
 }
